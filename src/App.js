6a748edb57e117052192a18b9d4d3dd5d9742e5c
kamrankhan0001 <kamranbgp15@gmail.com>
2023-08-22 01:48:57 +0530

    phone-book

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser]=useState('');
+  const [number, setNumber]=useState('');
+  const [showData, setShowData]=useState([]);
+
+  const handleClick =()=>{
+    setShowData([...showData,{user, number}])
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Phone Book</h1>
+      
+      <label>Enter the Name</label>
+      <br/><br/>
+      <input  type='text' value={user} onChange={(e)=>setUser(e.target.value)}></input>
+      <br/><br/>
+    
+      <label>Enter Phone number</label>
+      <br/><br/>
+      <input  type='text' value={number} onChange={(e)=>setNumber(e.target.value)}></input>
+      <br/><br/>
+      
+      <button onClick={handleClick}>Add contact</button>
+      <br/><br/>
+      <h2>Contacts</h2>
+      {/* using the map function to iterate over the showData array. 
+      It takes a callback function with data as the current element and i as the index. */}
+      {showData.map((data, i)=>{
+        return(
+          // Each mapped element needs a unique 'key' for efficient rendering
+          <div key={i}>
+            <li>{data.user} &nbsp; {data.number}</li>
+          </div>
+        )
+      })}
     </div>
   );
 }
